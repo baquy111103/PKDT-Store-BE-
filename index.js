bb356155ae3b3be76fd33b0728f61fb52160ffd8
baquy111103 <145130742+baquy111103@users.noreply.github.com>
2024-11-01 10:19:20 +0700

    done BE

--- index.js
+++ index.js
@@ -23,8 +23,13 @@ app.use(cors({
 
 // all routes
 const authRoutes = require('./src/users/user.route');
+const productRoutes = require('./src/products/products.route');
+const reviewRoutes = require('./src/reviews/reviews.router')
 
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/reviews', reviewRoutes);
+
 
 
 
